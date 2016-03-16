76f59616e1ebe57140a620156cd9bb26e6e28519
DmyP <pechenyi.d@gmail.com>
2016-03-16 15:42:57 +0200

    lab33

---
+++ juja/src/week5/lab33BigInteger/FractionUtils.java
@@ -0,0 +1,25 @@
+package week5.lab33BigInteger;
+/*
+    Предположим, что мы работаем с дробями, представляя их как пару BigInteger-ов.
+    2/3 будем представлять как BigInteger[] x = {new BigInteger("2"), new BigInteger("3")};
+    Реализовать метод, который обеспечивает - сложение дробей 1/3 + 1/3 = 2/3 1/2 + 1/3 = 5/6
+    - итоговая дробь должна быть несократимой
+    1/2 + 1/2 = 1/1 (а не 2/2)
+    2/3 - 1/6 = 1/2 (а не 3/6)
+    1/2 + (-1)/2 = 0/1 (а не 0/2) --- P.S. Возможно, вам потребуется метод BigInteger.gcd().
+*/
+
+import java.math.BigInteger;
+
+public class FractionUtils {
+    public static void main(String[] args) {
+
+    }
+
+    public static BigInteger[] sum(BigInteger[] x, BigInteger[] y) {
+        /*BODY*/
+        return null;
+    }
+
+
+}
