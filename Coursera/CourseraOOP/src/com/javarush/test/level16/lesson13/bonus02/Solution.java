df59dc41f32ff49e0dfa076190f7c94658940715
dmyp <pechenyi.d@gmail.com>
2016-04-06 23:50:42 +0300

    rush17

--- Coursera/CourseraOOP/src/com/javarush/test/level16/lesson13/bonus02/Solution.java
+++ Coursera/CourseraOOP/src/com/javarush/test/level16/lesson13/bonus02/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level16.lesson13.bonus02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,4 +20,89 @@ import java.util.List;
 
 public class Solution {
     public static List<Thread> threads = new ArrayList<Thread>(5);
+
+    static {
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
+    }
+
+    public static void main(String[] args) {
+        for (Thread thread: threads) {
+            thread.start();
+        }
+    }
+
+    public static class Thread1 extends Thread{
+        @Override
+        public void run() {
+            while (true) {
+            }
+        }
+    }
+
+    public static class Thread2 extends Thread{
+        @Override
+        public void run() {
+                try {
+                        while (!isInterrupted()) {
+
+                        }
+                        throw new InterruptedException();
+                }catch (InterruptedException e){
+                    System.out.println("InterruptedException");
+                }
+            }
+        }
+
+    public static class Thread3 extends Thread{
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Ура");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    public static class Thread4 extends Thread implements Message{
+
+        public void run() {
+            while (!isInterrupted()) {
+            }return;
+        }
+
+        public void showWarning() {
+            try{
+                interrupt();
+                join();
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public static class Thread5 extends Thread{
+
+        public void run() {
+            BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
+            String s;
+            int res = 0;
+            try {
+                while (!isInterrupted()) {
+                    if ((s = reader.readLine()).equals("N")) {
+                        System.out.println(res);
+                        interrupt();
+                    }else res = res + Integer.parseInt(s);
+                }
+                reader.close();
+
+            }catch(IOException e){
+            }
+        }
+    }
+
 }
