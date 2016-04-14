83b722325ad69b665e5ef690544b2462c3f9b342
dmyp <pechenyi.d@gmail.com>
2016-04-14 23:48:59 +0300

    rush19

--- Coursera/CourseraOOP/src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ Coursera/CourseraOOP/src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -1,6 +1,9 @@
-package com.javarush.test.level19.lesson10.bonus01;
+    package com.javarush.test.level19.lesson10.bonus01;
 
-import java.io.*;
+    import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,11 +21,11 @@ file1:         file2:             результат:(lines)
 строка3        строка3            SAME строка3
 строка4                           REMOVED строка4
 строка5        строка5            SAME строка5
-строка0                           ADDED строка0
+               строка0                           ADDED строка0
 строка1        строка1            SAME строка1
 строка2                           REMOVED строка2
 строка3        строка3            SAME строка3
-строка5                           ADDED строка5
+               строка5                           ADDED строка5
 строка4        строка4            SAME строка4
 строка5                           REMOVED строка5
 */
@@ -30,44 +33,50 @@ file1:         file2:             результат:(lines)
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader file = new BufferedReader(new InputStreamReader(System.in));
         String file1 = file.readLine();
         String file2 = file.readLine();
-        file.close();
-        BufferedReader bufferedReader1 =  new BufferedReader(new FileReader(file1));
-        BufferedReader bufferedReader2 =  new BufferedReader(new FileReader(file2));
-        String line1 = bufferedReader1.readLine();
-        String line2 = bufferedReader2.readLine();
-        while (line1 != null && line2 != null) {
-            if (line1.equals(line2))
-            {
-                LineItem item = new LineItem(Type.SAME, line1);
-                lines.add(item);
-            }else if (line2 == "")
-            {
-                LineItem item = new LineItem(Type.REMOVED, line1);
-                lines.add(item);
-            }else if (line1 == "")
-            {
-                LineItem item = new LineItem(Type.ADDED, line2);
-                lines.add(item);
-            }
-            line1 = bufferedReader1.readLine();
-            line2 = bufferedReader2.readLine();
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader(file1));
+        BufferedReader bufferedReader2 = new BufferedReader(new FileReader(file2));
+
+        List<String> line1 = new ArrayList<>();
+        while (bufferedReader1.ready()) {
+            line1.add(bufferedReader1.readLine());
         }
-        if (line1 == null && line2 != null)
-            diffs.add("File \"" + name2 + "\" has extra lines at line " +
-                    reader2.getLineNumber() + ":\n" + line2);
-        if (line1 != null && line2 == null)
-            diffs.add("File \"" + name1 + "\" has extra lines at line " +
-                    reader1.getLineNumber() + ":\n" + line1);
+        List<String> line2 = new ArrayList<>();
+        while (bufferedReader2.ready()) {
+            line2.add(bufferedReader2.readLine());
+        }
+
+        int j = 0;
+        for (int i = 0; i < line1.size(); i++){
+            if (j >= line2.size()) {
+                lines.add(new LineItem(Type.REMOVED, line1.get(i)));
+                break;
+            } else
+            if (line1.get(i).equals(line2.get(j))) {
+                lines.add(new LineItem(Type.SAME, line1.get(i)));
+                j++;
+            } else
+            if (j < (line2.size() - 1) && line1.get(i).equals(line2.get(j + 1))) {
+                lines.add(new LineItem(Type.ADDED, line2.get(j)));
+                j++;
+                i--;
+            } else
+            if (i < (line1.size() - 1) && line1.get(i + 1).equals(line2.get(j))) {
+                lines.add(new LineItem(Type.REMOVED, line1.get(i)));
+            }
+        }
+        if (lines.get(lines.size() - 1).type == Type.SAME && j <= (line2.size() - 1) ) {
+            lines.add(new LineItem(Type.ADDED, line2.get(j)));
+        }
+        bufferedReader1.close();
+        bufferedReader2.close();
     }
 
 
-
-
-    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
