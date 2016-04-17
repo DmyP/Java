8cfb6a8c8f5cad3d5e02306698c2da82026608db
dmyp <pechenyi.d@gmail.com>
2016-04-17 13:44:13 +0300

    rush21

---
+++ hints.java
@@ -0,0 +1,3 @@
+    int min = x < y ? x < z ? x : z : y < z ? y : z;
+            returnString = "Thank you " + (person.isMale() ? "Mr. " : "Ms. ") + person.getLastName() + ".";
+            return a!=null ? a.length : 0;
