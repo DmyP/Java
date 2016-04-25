990133aac558854cbeb79230d17e7a1cd7e9fa14
DmyP <pechenyi.d@gmail.com>
2016-04-25 16:03:06 +0300

    rush27

--- Coursera/CourseraOOP/src/com/javarush/test/level27/lesson15/big01/Tablet.java
+++ Coursera/CourseraOOP/src/com/javarush/test/level27/lesson15/big01/Tablet.java
@@ -3,14 +3,39 @@ package com.javarush.test.level27.lesson15.big01;
 import com.javarush.test.level27.lesson15.big01.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable{
     private final int number;
+    final static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
-    public void createOrder() throws IOException {
-        Order order = new Order();
+
+    public void createOrder() {
+        Order order = null;
+        try {
+            order = new Order(this);
+            if (!order.isEmpty()) {
+                ConsoleHelper.writeMessage(order.toString());
+                setChanged();
+                notifyObservers(order);
+            }
+        }
+        catch (IOException e) {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
+
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Tablet{number=" + number + "}";
     }
 }
--- Coursera/CourseraOOP/src/com/javarush/test/level27/lesson15/big01/kitchen/Order.java
+++ Coursera/CourseraOOP/src/com/javarush/test/level27/lesson15/big01/kitchen/Order.java
@@ -1,23 +1,35 @@
 package com.javarush.test.level27.lesson15.big01.kitchen;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level27.lesson15.big01.Tablet;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+
     private Tablet tablet;
     private List<Dish> dishes;
-    private List<Dish> orderedDishes;
 
     public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
-        this.dishes = new ArrayList<>();
-        this.orderedDishes = new ArrayList<>();
+        dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+    public boolean isEmpty(){
+        return dishes.isEmpty();
     }
 
-    public Order() {
+    public int getTotalCookingTime(){
+        int duration = 0;
+        for (int i = 0; i < dishes.size(); i++) {
+            duration += dishes.get(i).getDuration();
+        }
+        return duration;
+    }
 
+
+    @Override
+    public String toString() {
+        return dishes.isEmpty() ? "": "Your order: " + dishes + " of " + tablet;
     }
 }
