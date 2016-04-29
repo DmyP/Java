dde95aa3a941f97038f5359cfcb2aa1e5a2f4d19
DmyP <pechenyi.d@gmail.com>
2016-04-29 22:06:50 +0300

    rush28

---
+++ Coursera/CourseraOOP/src/com/javarush/test/level29/lesson15/big01/car/Cabriolet.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level29.lesson15.big01.car;
+
+public class Cabriolet extends Car {
+    protected Cabriolet(int type, int numberOfPassengers) {
+        super(type, numberOfPassengers);
+    }
+}
---
+++ Coursera/CourseraOOP/src/com/javarush/test/level29/lesson15/big01/car/Truck.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level29.lesson15.big01.car;
+
+public class Truck extends Car {
+    protected Truck(int type, int numberOfPassengers) {
+        super(type, numberOfPassengers);
+    }
+}
