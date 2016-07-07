611b3593d8d9b269985f5ac5d18434b70ff05904
dmyp <pechenyi.d@gmail.com>
2016-07-07 10:12:21 +0300

    rush34

--- Coursera/CourseraGraphs/src/com/javarush/test/level35/lesson10/bonus01/Solution.java
+++ Coursera/CourseraGraphs/src/com/javarush/test/level35/lesson10/bonus01/Solution.java
@@ -1,10 +1,13 @@
 package com.javarush.test.level35.lesson10.bonus01;
 
 import java.io.File;
-import java.nio.file.*;
-import java.util.ArrayList;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /* ClassLoader - что это такое?
@@ -17,62 +20,84 @@ import java.util.Set;
 Добавить созданные объекты в результирующий сет и вернуть.
 Метод main не участвует в тестировании.
 */
-public class Solution {
-    public static void main(String[] args) {
-        Set<? extends Animal> allAnimals = getAllAnimals("C://pathToClasses/");
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //Set<? extends Animal> allAnimals = getAllAnimals("C://pathToClasses/");
+        Set<? extends Animal> allAnimals = getAllAnimals("/Users/inna/Documents/Java/Coursera/CourseraGraphs/src/com/javarush/test/level35/lesson10/bonus01/data");
         System.out.println(allAnimals);
     }
 
-    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        if (pathToAnimals == null) return null;
-        if (!pathToAnimals.endsWith("/") && !pathToAnimals.endsWith("\\")) pathToAnimals += "/";
-
+    public static Set<? extends Animal> getAllAnimals(String pathToAnimals)
+    {
         Set<Animal> animals = new HashSet<>();
-        File files = new File(pathToAnimals);
+        if (!pathToAnimals.endsWith("\\") && !pathToAnimals.endsWith("/"))
+            pathToAnimals = pathToAnimals + "/";
 
-        List<Path> classFiles = pathParser(pathToAnimals);
-
-
-
-
-
-        return null;
-    }
-    private static List<Path> walkFileTree(String pathToAnimals) {
-        List<Path> files = new ArrayList<>();
-        try {
-            Files.walkFileTree(Paths.get(pathToAnimals), new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (file.toString().endsWith(".class")) {
-                        files.add(file);
+        File dirOfClass = new File(pathToAnimals);
+        String[] classList = dirOfClass.list(new FilenameFilter()
+        {
+            @Override
+            public boolean accept(File dir, String name)
+            {
+                return name.toLowerCase().endsWith(".class");
+            }
+        });
+        for (String m : classList)
+        {
+            try
+            {
+                final String finalPathToAnimals = pathToAnimals;
+                ClassLoader loader = new ClassLoader()
+                {
+                    @Override
+                    protected Class<?> findClass(String className) throws ClassNotFoundException
+                    {
+                        try
+                        {
+                            byte b[] = Files.readAllBytes(Paths.get(finalPathToAnimals + className + ".class"));
+                            return defineClass(null, b, 0, b.length);
+                        }
+                        catch (IOException e)
+                        {
+                            return super.findClass(className);
+                        }
                     }
-                    return FileVisitResult.CONTINUE;
+                };
+                String mName = m.substring(0, m.length()-6);
+                Class clazz = loader.loadClass(mName);
+                boolean hasInterface = false;
+                Class[] interfaces = clazz.getInterfaces();
+                for (Class i : interfaces)
+                {
+                    if (Animal.class == i)
+                    {
+                        hasInterface = true;
+                        break;
+                    }
                 }
-            });
-
-            return files;
-        }
-        catch (IOException e) {
-            return null;
-        }
-        return null;
-    }
-    private static class MyClassLoader extends ClassLoader {
-        private Set<Class> classes = new HashSet<>();
-        public void loadClasses(List<Path> paths) {
-            try {
-                for (Path path: paths) {
-                    byte[] bytes = Files.readAllBytes(path);
-                    Class<?> defineClass = defineClass(null, bytes, 0, bytes.length);
-                    this.classes.add(defineClass);
+                if (!hasInterface) continue;
+                boolean hasConstructor = false;
+                Constructor[] constructors = clazz.getConstructors();
+                for (Constructor c : constructors)
+                {
+                    if (Modifier.isPublic(c.getModifiers()) && c.getParameterTypes().length == 0)
+                    {
+                        hasConstructor = true;
+                        break;
+                    }
                 }
-            } catch (Exception e) {}
+                if (!hasConstructor) continue;
+                animals.add((Animal) clazz.newInstance());
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
         }
-        public Set<Class> getClasses() {
-            return classes;
-        }
+
+
+        return animals;
     }
-
-
 }
